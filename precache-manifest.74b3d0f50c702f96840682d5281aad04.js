25246185f5616cb211700f4d5528284c0e5ba544
Arnaud De Boeck <deboeck.arnaud@gmail.com>
2020-03-06 21:14:54 +0100

    Updates

---
+++ precache-manifest.74b3d0f50c702f96840682d5281aad04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57f0f9b5d2d38b3ac0085d124fe76ecc",
+    "url": "/monsters-react-udemy/index.html"
+  },
+  {
+    "revision": "47e23ab24b616a33767b",
+    "url": "/monsters-react-udemy/static/css/main.aba815dc.chunk.css"
+  },
+  {
+    "revision": "2317dcccfdb52416c501",
+    "url": "/monsters-react-udemy/static/js/2.a96e9d04.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-react-udemy/static/js/2.a96e9d04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47e23ab24b616a33767b",
+    "url": "/monsters-react-udemy/static/js/main.7843b130.chunk.js"
+  },
+  {
+    "revision": "22587d76156271ec36b4",
+    "url": "/monsters-react-udemy/static/js/runtime-main.a340adc7.js"
+  }
+]);
